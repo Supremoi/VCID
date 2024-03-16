548321513368b240ac58d7cd87cef404066640dc
Supremoi <ricardo.ottaviani@student.ipso.ch>
2024-03-16 16:53:00 +0100

    end chapter 2

---
+++ zwitscher.py
@@ -0,0 +1,2 @@
+## Importiert die Flask-Anwendung 'app' aus dem 'app'-Paket zur Verwendung in diesem Modul, wobei 'app' sowohl der Name des Pakets als auch der Instanzvariable ist.
+from app import app
