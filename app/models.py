f816ab1ce0498b238ddffd60766e0e4c66f854cc
Supremoi <ricardo.ottaviani@student.ipso.ch>
2024-03-17 16:46:39 +0100

    ENDE REGISTRIERUNG

--- app/models.py
+++ app/models.py
@@ -1,18 +1,30 @@
-from app import db 
+from app import db, login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     userid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), index=True, unique=True)
     email = db.Column(db.String(255), index=True, unique=True)
     password_hash = db.Column(db.String(255))
+    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-    # Repräsentationsmethode
-    def __repr__(self):
-        return f'<User {self.username}>'
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-    # Beziehungen
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
-    comments = db.relationship('Comment', backref='commenter', lazy='dynamic')
+# Repräsentationsmethode
+def __repr__(self):
+    return f'<User {self.username}>'
+
+# Beziehungen
+posts = db.relationship('Post', backref='author', lazy='dynamic')
+comments = db.relationship('Comment', backref='commenter', lazy='dynamic')
 
 class Post(db.Model):
     postid = db.Column(db.Integer, primary_key=True)
